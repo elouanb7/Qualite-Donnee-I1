2e95645f07701fafc0eee81e1a6180bc48037425
elouanb7 <elouanb7@gmail.com>
2023-10-17 17:56:43 +0200

    WIP : implementation of stop_times.json

--- src/components/utils/parser.js
+++ src/components/utils/parser.js
@@ -10,8 +10,22 @@ export async function parseStops() {
       return jsonData.map((stop) => ({
         coordinates: fromLonLat([stop.stop_lon, stop.stop_lat]),
         name: stop.stop_name,
-        id: stop.stop_id
+        id: stop.stop_id,
+        wheelchair: stop.wheelchair_boarding
       }))
+    })
+    .catch((error) => {
+      console.error('Error fetching JSON data:', error)
+      return []
+    })
+}
+
+export async function parseStopTimes() {
+  return axios
+    .get('src/data/stop_times.json')
+    .then((response) => {
+      let jsonData = response.data
+      console.log('first :', jsonData[0])
     })
     .catch((error) => {
       console.error('Error fetching JSON data:', error)
