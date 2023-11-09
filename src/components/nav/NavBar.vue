959e024b7ba53bf109f702b50176f0a01043072d
txomac <thomas.hnf@gmail.com>
2023-11-09 09:37:18 +0100

    add nav and about page

---
+++ src/components/nav/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav>
+    <div id="left">
+      <RouterLink to="/">NaviMap</RouterLink>
+    </div>
+    <div id="center">
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+  text-align: center;
+  margin: 2rem 0 2rem;
+  font-size: 18px;
+  display: flex;
+  justify-content: space-around;
+}
+
+#left {
+  font-size: 25px;
+  width: 10rem;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+}
+
+#center {
+  display: flex;
+  align-items: center;
+}
+
+nav a.router-link-exact-active {
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  text-decoration: none;
+  color: black;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
