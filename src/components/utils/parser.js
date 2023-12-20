bc2328f61e6954380a6615b12347429ea8774b96
elouanb7 <elouanb7@gmail.com>
2023-12-20 14:12:31 +0100

    Edited Routes

--- src/components/utils/parser.js
+++ src/components/utils/parser.js
@@ -6,7 +6,7 @@ export async function parseStops() {
     .get('src/data/stops.json')
     .then(async (response) => {
       let stops = response.data
-      stops = stops.filter((element) => element.stop_id.length === 4)
+      stops = await combineStops(stops)
       const shapes = await parseRawShapes()
       stops = await associateShapesStops(shapes, stops)
       return stops.map((stop) => ({
@@ -21,6 +21,51 @@ export async function parseStops() {
       console.error('Error fetching JSON data:', error)
       return []
     })
+}
+
+export async function parseStopsFromAPI() {
+  return axios
+    .get('https://open.tan.fr/ewp/arrets.json')
+    .then(async (response) => {
+      let stops = response.data
+      return stops
+      /*const shapes = await parseRawShapes()
+      stops = await associateShapesStops(shapes, stops)
+      return stops.map((stop) => ({
+        coordinates: fromLonLat([stop.stop_lon, stop.stop_lat]),
+        name: stop.stop_name,
+        id: stop.stop_id,
+        wheelchair: stop.wheelchair_boarding,
+        routes: stop.routes
+      }))*/
+    })
+    .catch((error) => {
+      console.error('Error fetching API data:', error)
+      return []
+    })
+}
+
+export async function combineStops(stops) {
+  const apiStops = await parseStopsFromAPI()
+  stops = new Map(stops.map((stop) => [stop.stop_id, stop]))
+
+  return apiStops
+    .map((apiStop) => {
+      const stop = stops.get(apiStop.codeLieu)
+      if (stop) {
+        return {
+          stop_id: apiStop.codeLieu,
+          stop_name: apiStop.libelle,
+          stop_lat: stop.stop_lat,
+          stop_lon: stop.stop_lon,
+          wheelchair: stop.wheelchair_boarding,
+          routes: apiStop.ligne.map((ligne) => ligne.numLigne)
+        }
+      } else {
+        return null
+      }
+    })
+    .filter(Boolean)
 }
 
 export async function parseRoutes() {
@@ -81,7 +126,24 @@ async function associateShapesStops(shapesData, stopsData) {
     ])
   )
 
-  shapesData.forEach((shape) => {
+  for (const stop of stopsData) {
+    stop.routes = stop.routes
+      .map((route) => {
+        const routeValues = routesMap.get(route)
+        if (routeValues) {
+          const [routeColor, routeTextColor] = routeValues
+          return {
+            route_name: route,
+            route_color: routeColor,
+            route_text_color: routeTextColor
+          }
+        }
+        return null
+      })
+      .filter(Boolean)
+  }
+
+  /*shapesData.forEach((shape) => {
     const key = `${shape.shape_pt_lat}:${shape.shape_pt_lon}`
     if (!shapeIdMap.has(key)) {
       shapeIdMap.set(key, [])
@@ -108,17 +170,19 @@ async function associateShapesStops(shapesData, stopsData) {
       }
       // Add the pair to the shape_ids array if it's not already included
       if (!shapeIdMap.get(key).some(({ route }) => route === shapeId)) {
+        console.log(shapeIdMap.get(key))
         shapeIdMap.get(key).push(pair)
+        console.log(shapeIdMap.get(key))
       }
     }
   })
-
   for (let i = 0; i < stopsData.length; i++) {
     const stop = stopsData[i]
     const key = `${stop.stop_lat}:${stop.stop_lon}`
     if (shapeIdMap.has(key)) {
       stop.routes = shapeIdMap.get(key)
     }
-  }
+  }*/
+  console.log('teqsdjfbzshfgbzjhbz', stopsData, routesMap)
   return stopsData
 }
